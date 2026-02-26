bd52cab06255813c126fcc6cb6dbafd105f8763d
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-02-26 13:07:14 -0500

    I fixed the pin number

--- main.ts
+++ main.ts
@@ -8,12 +8,12 @@
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P16, 0)
 
 input.onButtonPressed(Button.A, function() {
-    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
 })
 
 input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
 })
