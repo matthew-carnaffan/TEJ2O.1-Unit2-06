a3f84308fa80b01d8adc4e31298b5a509d17aee1
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-02-26 13:06:09 -0500

    I made a basic code for toggling a LED on and off.

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Matthew Carnaffan
+ * Created on: Feb 2026
+ * This program turns on a LED on command,then off on command.
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+pins.digitalWritePin(DigitalPin.P0, 0)
+
+input.onButtonPressed(Button.A, function() {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
+
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
