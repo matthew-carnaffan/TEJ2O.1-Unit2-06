387c8a5ae8e2f6190b352ab8869859a6c3f090b8
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-02-26 13:10:31 -0500

    I made the interface more user friendly.

--- main.ts
+++ main.ts
@@ -11,9 +11,15 @@ basic.showIcon(IconNames.Happy)
 pins.digitalWritePin(DigitalPin.P16, 0)
 
 input.onButtonPressed(Button.A, function() {
+    basic.showIcon(IconNames.Yes)
     pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(3000)
+    basic.showIcon(IconNames.Happy)
 })
 
 input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
     pins.digitalWritePin(DigitalPin.P16, 0)
+    basic.pause(3000)
+    basic.showIcon(IconNames.Happy)
 })
